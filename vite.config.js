541f407062b2467235605785c1958feea2f78135
francxrusso <98696732+francxrusso@users.noreply.github.com>
2023-12-11 12:03:04 +0100

    up

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tic-tac-toe-s2i/",
+  base: '/tic-tac-toe-s2i/',
   plugins: [vue()],
 });
