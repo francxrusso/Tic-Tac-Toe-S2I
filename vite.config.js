11d26f7c59e531b09edfd8fa8e615e1d98b5e66f
Fran <98696732+francxrusso@users.noreply.github.com>
2023-12-11 13:38:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tic-tac-toe-s2i/',
-  plugins: [createVuePlugin(), vue()],
+  plugins: [vue()],
 });
