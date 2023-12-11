c856951895984578df22d23df1570dba78747ab9
francxrusso <98696732+francxrusso@users.noreply.github.com>
2023-12-11 12:36:04 +0100

    updates

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import App from "../../App.vue";
-import "../css/global.css";
+import App from "./App.vue";
+import "./assets/css/global.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
